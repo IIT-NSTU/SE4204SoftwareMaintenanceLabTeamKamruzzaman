00a9d5aacbaae657eb54bb7d112bc13a077d7fcf
Shuvra Aditya <sadihurayv@gmail.com>
2023-03-27 02:16:12 +0600

    Fix 'Large Class' with 'Extract Class'.

--- ChemistryCalculator/backend/Atom.java
+++ ChemistryCalculator/backend/Atom.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class Atom {
     private static final HashMap<String, String[]> allAtoms = new Database().getAllAtoms();
+    private final Orbitals orbitals = new Orbitals(this);
     private String symbol;
     private String name;
     private  double atomicMass;
@@ -47,37 +48,7 @@ public class Atom {
 
 
     public String getElectronConfig() {
-
-        int counter;
-        int tempAtomicNumber = this.atomicNumber;
-
-        StringBuilder output = new StringBuilder();
-        String[][] orbitals = {
-                {"1s", "2"}, {"2s", "2"}, {"2p", "6"},
-                {"3s", "2"}, {"3p", "6"}, {"4s", "2"},
-                {"3d", "10"}, {"4p", "6"}, {"5s", "2"},
-                {"4d", "10"}, {"5p", "6"}, {"6s", "2"},
-                {"4f", "14"}, {"5d", "10"}, {"6p", "6"},
-                {"7s", "2"}, {"5f", "14"}, {"6d", "10"},
-                {"7p", "6"}, {"8s", "2"}
-        };
-
-        for (String[] innerArray : orbitals) {
-            String orbitalName = innerArray[0];
-            int orbitalSize = Integer.parseInt(innerArray[1]);
-
-            counter = Math.min(orbitalSize, tempAtomicNumber);
-
-            output.append(orbitalName).append(counter).append(" ");
-
-            tempAtomicNumber -= counter;
-            if (tempAtomicNumber <= 0) {
-                break;
-            }
-
-        }
-
-        return output.toString();
+        return orbitals.getElectronConfig(this.atomicNumber);
     }
 
     public String getSymbol() {
---
+++ ChemistryCalculator/backend/Orbitals.java
@@ -0,0 +1,42 @@
+package ChemistryCalculator.backend;
+
+public class Orbitals {
+    private final Atom atom;
+
+    public Orbitals(Atom atom) {
+        this.atom = atom;
+    }
+
+    public String getElectronConfig(int atomicNumber) {
+
+        int counter;
+
+        StringBuilder output = new StringBuilder();
+        String[][] orbitals = {
+                {"1s", "2"}, {"2s", "2"}, {"2p", "6"},
+                {"3s", "2"}, {"3p", "6"}, {"4s", "2"},
+                {"3d", "10"}, {"4p", "6"}, {"5s", "2"},
+                {"4d", "10"}, {"5p", "6"}, {"6s", "2"},
+                {"4f", "14"}, {"5d", "10"}, {"6p", "6"},
+                {"7s", "2"}, {"5f", "14"}, {"6d", "10"},
+                {"7p", "6"}, {"8s", "2"}
+        };
+
+        for (String[] innerArray : orbitals) {
+            String orbitalName = innerArray[0];
+            int orbitalSize = Integer.parseInt(innerArray[1]);
+
+            counter = Math.min(orbitalSize, atomicNumber);
+
+            output.append(orbitalName).append(counter).append(" ");
+
+            atomicNumber -= counter;
+            if (atomicNumber <= 0) {
+                break;
+            }
+
+        }
+
+        return output.toString();
+    }
+}
